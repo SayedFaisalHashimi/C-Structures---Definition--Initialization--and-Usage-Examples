56f9f2a2a22ff24516f1962e72604a3f62c3653d
Sayed Faisal Hashimi <140006205+SayedFaisalHashimi@users.noreply.github.com>
2025-11-05 12:17:48 +0300

    Add structure copy example demonstrating assignment between structure variables
    - Added example showing how to copy one structure's data to another using direct assignment
    - Demonstrated printing values of both structures to verify successful data copy
    - Improved formatting and readability of output.

--- Structure_Initialization_and_Member_Access.c
+++ Structure_Initialization_and_Member_Access.c
@@ -12,14 +12,23 @@ struct student
 int main() 
 {
     struct student s1;
+    struct student s2;
 
     s1.id = 14678945;
     strcpy(s1.name, "Hashimi");
     s1.age = 22;
     s1.gpa = 3.60;
 
+
+    // Copy structure s1 into s2
+    s2 = s1;
+
     printf("ID = %d\nName = %s\nAge = %d\nGPA = %.2f\n", 
            s1.id, s1.name, s1.age, s1.gpa);
+
+
+           printf("Student 2 (Copied from Student 1):\n");
+    printf("ID = %d\nName = %s\nAge = %d\nGPA = %.2f\n", s2.id, s2.name, s2.age, s2.gpa);
 
     return 0;
 }
